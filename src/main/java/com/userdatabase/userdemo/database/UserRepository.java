6e317dfe0b175c395e9c18aa4b0dac558177f560
irasinha04 <irasinha.04@gmail.com>
2018-09-13 20:34:18 +0530

    Add UserRepository and a dependency

---
+++ src/main/java/com/userdatabase/userdemo/database/UserRepository.java
@@ -0,0 +1,9 @@
+package com.userdatabase.userdemo.database;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.userdatabase.userdemo.entity.User;
+
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
